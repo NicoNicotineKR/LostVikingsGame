8364d104d9b00a2f9e711910e7b6686dea0450c2
Zeniz <zeniz.sians@gmail.com>
2019-01-20 03:06:14 +0900

    Revert "add Intro and Menu"
    This reverts commit 4fdef9323278b7f07e1c6f730ca5f1839d252e04.

--- invenUI.h
+++ invenUI.h
@@ -5,6 +5,11 @@ class invenUI : public gameNode
 {
 private:
 
+	enum E_TMP_CHAR_IDX {		// tmpIdx, It 
+		E_ERIC,
+		E_BALEOG,
+		E_OLAF
+	};
 
 
 public:
--- playGround.h
+++ playGround.h
@@ -1,9 +1,6 @@
 #pragma once
 #include "gameNode.h"
 #include "scene2_1.h"
-#include "title.h"
-#include "mainmenu.h"
-
 
 //추가
 #include "camera.h"
@@ -12,18 +9,9 @@
 class playGround : public gameNode
 {
 private:
-
-	title* _title;
-	mainmenu* _mainmenu;
-
 	scene2_1* _scene2_1;
 	//추가
 	camera* _camera;
-
-
-	E_SCENE_INDEX _curSceneIdx;
-
-
 
 	char str[128];
 
@@ -35,8 +23,6 @@ public:
 	virtual void release();
 	virtual void update();
 	virtual void render();
-
-	void SelectSceneFunc();		// 씬 끝났다는 신호 받아서, 다음씬으로 넘김
 
 
 
